4d9a05b8e0209fd8d67a4b48b82a9b4a88b81d99
kevinmoral.es <hello@kevinmoral.es>
2023-02-10 16:41:34 -0600

    fixing stupid cli

--- angular-15-client/src/app/services/paprika.service.ts
+++ angular-15-client/src/app/services/paprika.service.ts
@@ -1,41 +1,20 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
-import { Tutorial } from '../models/tutorial.model';
+import { Observable, take } from 'rxjs';
 
-const baseUrl = 'https://www.paprikaapp.com/api/v2/account/login/';
+const baseUrl = 'http://localhost:8080/getRecipes';
 
 @Injectable({
   providedIn: 'root',
 })
-export class TutorialService {
-  constructor(private http: HttpClient) {}
-
-  getAll(): Observable<Tutorial[]> {
-    return this.http.get<Tutorial[]>(baseUrl);
+export class PaprikaService {
+  constructor(private http: HttpClient) {
+    this.getAll()
+      .pipe(take(1))
+      .subscribe((recipes) => console.log(recipes));
   }
 
-  get(id: any): Observable<Tutorial> {
-    return this.http.get<Tutorial>(`${baseUrl}/${id}`);
-  }
-
-  create(data: any): Observable<any> {
-    return this.http.post(baseUrl, data);
-  }
-
-  update(id: any, data: any): Observable<any> {
-    return this.http.put(`${baseUrl}/${id}`, data);
-  }
-
-  delete(id: any): Observable<any> {
-    return this.http.delete(`${baseUrl}/${id}`);
-  }
-
-  deleteAll(): Observable<any> {
-    return this.http.delete(baseUrl);
-  }
-
-  findByTitle(title: any): Observable<Tutorial[]> {
-    return this.http.get<Tutorial[]>(`${baseUrl}?title=${title}`);
+  getAll(): Observable<any> {
+    return this.http.get<any[]>(`${baseUrl}/getRecipes`);
   }
 }
--- node-express-server/server.js
+++ node-express-server/server.js
@@ -13,23 +13,22 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 let paprika = new PaprikaApi(process.env.PAPRIKA_USER, process.env.PAPRIKA_PASS);
 
-const getRecipes = paprika
-	.recipes()
-	.then((recipes) => recipes)
-	.then((recipes) => console.log(recipes))
-	.catch((err) => console.error(err));
+const getRecipeItems = () =>
+	paprika
+		.recipes()
+		.then((recipes) => recipes)
+		.catch((err) => console.error(err));
 
-const getRecipe = paprika.recipes().then((recipes) => {
-	paprika.recipe(recipes[0].uid).then((recipe) => {
-		console.log(recipe);
-	});
-});
+const recipeIds = await getRecipeItems().then((ids) => ids.map((ids) => ids.uid));
+const getRecipe = async (uid) => await paprika.recipe(uid).then((recipe) => console.log(recipe));
 
-app.post('/getRecipes', async (req, res) => res.send(await getRecipes()));
+const allRecipes = recipeIds.map(async (id) => await getRecipe(id));
+
+
+
+app.post('/getRecipes', async (req, res) => res.send(allRecipes));
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
 	console.log(`Server is running on port ${PORT}.`);
 });
-
-// '06E4036A-C81F-4CC6-B1B7-F39AD9ACF816';
