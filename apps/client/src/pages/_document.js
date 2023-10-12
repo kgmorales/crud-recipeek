59b5cbd956f6850eed80e9bffb98cf2f884484a1
kevinmoral.es <hello@kevinmoral.es>
2023-10-12 13:35:36 -0500

    added favicon

--- apps/client/src/pages/_document.js
+++ apps/client/src/pages/_document.js
@@ -1,22 +1,27 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document'
+import Document, { Html, Head, Main, NextScript } from 'next/document';
 
 class MyDocument extends Document {
   render() {
     return (
       <Html>
         <Head>
-            <link
-                href="https://fonts.bunny.net/css?family=noto-sans:400,500,700,800"
-                rel="stylesheet"
-            />
+          <link
+            href="https://fonts.bunny.net/css?family=noto-sans:400,500,700,800"
+            rel="stylesheet"
+          />
+          <link
+            rel="icon"
+            type="image/svg+xml"
+            href="/assets/imgs/lamora-logo-night.svg"
+          />
         </Head>
         <body>
           <Main />
           <NextScript />
         </body>
       </Html>
-    )
+    );
   }
 }
 
-export default MyDocument
+export default MyDocument;
