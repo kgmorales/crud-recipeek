cc701f6ba79c3c581dffe9ddda77e98203f7e9d7
kevinmoral.es <hello@kevinmoral.es>
2023-10-25 21:32:18 -0500

    styling

--- apps/client/src/components/elements/featured-card/FeaturedCard.tsx
+++ apps/client/src/components/elements/featured-card/FeaturedCard.tsx
@@ -14,7 +14,7 @@ const CardComponent: React.FC<FeaturedCardProps> = ({ cardInfo }) => {
   return (
     <div className={styles.container}>
       <div
-        className={styles.card}
+        className={`${styles.card} hover-up hover-neon wow animate__ animate__fadeInUp animated`}
         style={{
           backgroundImage: `url(${cardInfo.image_url})`,
           backgroundSize: 'cover',
@@ -25,54 +25,52 @@ const CardComponent: React.FC<FeaturedCardProps> = ({ cardInfo }) => {
         <div className={styles.content}>
           <h2 className={styles.title}>{cardInfo.name}</h2>
           <p className={styles.notes}>{cardInfo.description}</p>
-          <div>
-            <ul className={styles.meta}>
-              <li>
-                <Image
-                  className="d-none logo-day"
-                  src={`${iconPATH}/stopwatch-day.svg`}
-                  alt="icon"
-                  width={25}
-                  height={25}
-                />
-                <Image
-                  className="logo-night"
-                  src={`${iconPATH}/stopwatch.svg`}
-                  alt="icon"
-                  width={25}
-                  height={25}
-                />
-                <div className={styles.numberContainer}>
-                  {cardInfo.prep_time}
-                </div>
-              </li>
-              <li>
-                <Image
-                  className="d-none logo-day"
-                  src={`${iconPATH}/list-check-day.svg`}
-                  alt="icon"
-                  width={25}
-                  height={25}
-                />
-                <Image
-                  className="logo-night"
-                  src={`${iconPATH}/list-check.svg`}
-                  alt="icon"
-                  width={25}
-                  height={25}
-                />
-                <div className={styles.numberContainer}>
-                  {cardInfo.ingredientsCount} ingredients
-                </div>
-              </li>
-            </ul>
-          </div>
+
+          <ul className={styles.meta}>
+            <li>
+              <Image
+                className="d-none logo-day"
+                src={`${iconPATH}/stopwatch-day.svg`}
+                alt="icon"
+                width={25}
+                height={25}
+              />
+              <Image
+                className="logo-night"
+                src={`${iconPATH}/stopwatch.svg`}
+                alt="icon"
+                width={25}
+                height={25}
+              />
+              <div className={styles.numberContainer}>{cardInfo.prep_time}</div>
+            </li>
+            <li>
+              <Image
+                className="d-none logo-day"
+                src={`${iconPATH}/list-check-day.svg`}
+                alt="icon"
+                width={25}
+                height={25}
+              />
+              <Image
+                className="logo-night"
+                src={`${iconPATH}/list-check.svg`}
+                alt="icon"
+                width={25}
+                height={25}
+              />
+              <div className={styles.numberContainer}>
+                {cardInfo.ingredientsCount} ingredients
+              </div>
+            </li>
+          </ul>
+
           <div className="text-center mt-30">
             <Link
               className="btn btn-linear btn-load-more wow animate__animated animate__zoomIn"
               href="{cardInfo.recipeLink}"
             >
-              Show Full Recipe
+              Full Recipe
             </Link>
           </div>
         </div>
--- apps/client/src/components/layout/Header.tsx
+++ apps/client/src/components/layout/Header.tsx
@@ -32,10 +32,10 @@ const tags: Tag[] = [
 
 const navLinks: Link[] = [
   { href: '/', label: 'Home', className: 'active', key: 1 },
-  { href: '/about', label: 'About', className: 'color-gray-500', key: 2 },
-  { href: '/blog', label: 'Blog', className: 'color-gray-500', key: 3 },
-  { href: '/recipes', label: 'Recipes', className: 'color-gray-500', key: 4 },
-  { href: '/contact', label: 'Contact', className: 'color-gray-500', key: 5 },
+  { href: '/about', label: 'About', className: 'color-gray-300', key: 2 },
+  { href: '/blog', label: 'Blog', className: 'color-gray-300', key: 3 },
+  { href: '/recipes', label: 'Recipes', className: 'color-gray-300', key: 4 },
+  { href: '/contact', label: 'Contact', className: 'color-gray-300', key: 5 },
 ];
 
 const Header: React.FC<HeaderProps> = ({
@@ -91,7 +91,7 @@ const Header: React.FC<HeaderProps> = ({
                   width={50}
                   height={50}
                 />
-                <h6 className="color-gray-300 d-flex justify-center align-items-bottom">
+                <h6 className="color-gray-100 d-flex justify-center align-items-bottom">
                   laMora
                 </h6>
               </Link>
@@ -150,7 +150,7 @@ const Header: React.FC<HeaderProps> = ({
                     <Link
                       key={tag.key}
                       href={tag.href}
-                      className="color-gray-500 mr-10 font-xs"
+                      className="color-gray-300 mr-10 font-xs"
                     >
                       #{tag.label},
                     </Link>
